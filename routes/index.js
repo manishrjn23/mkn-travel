2bcba63d579570d6e4fe38c5fc4f74917098b8f4
Manish Rajani <manishrjn23@gmail.com>
2020-11-22 17:59:44 +0530

    basic search implemented

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { ensureAuthenticated } = require("../config/auth");
 const router = express.Router();
+router.get('/dashboard',ensureAuthenticated,(req,res)=>{
+    res.render('dashboard')
+})
 
 router.get('/',(req,res)=>{
     res.render('hello')
--- routes/organizations.js
+++ routes/organizations.js
@@ -1,85 +1,97 @@
 const express = require("express");
 const { ensureAuthenticated } = require("../config/auth");
 const router = express.Router();
-const Organizations=require('../models/Organizations')
-const Ratings=require('../models/Ratings')
-const mongoose=require('mongoose');
+const Organizations = require("../models/Organizations");
+const Ratings = require("../models/Ratings");
+const mongoose = require("mongoose");
 const { render } = require("ejs");
 const e = require("express");
 
-//main page
-router.get('/',ensureAuthenticated,(req,res)=>{
-    res.render('dashboard')
-})
-
-
 //item specific page
 
-// router.get('/:id',ensureAuthenticated,(req,res)=>{
-//     id=req.params.id;
-//     
-// })
-
-
-
+router.get("/info/:id", ensureAuthenticated, (req, res) => {
+  id = mongoose.Types.ObjectId(req.params.id);
+  Organizations.findById(id, function (err, org) {
+    if (err) throw err;
+    else {
+      res.render("org-page", { org });
+    }
+  });
+});
 
 //display all items
-router.get('/explore',ensureAuthenticated,(req,res)=>{
-    Organizations.find().exec(function(err,orgs){
-        if(err) throw err;
-        else{
-            res.render('explore',{orgs});
+router.get("/explore", ensureAuthenticated, (req, res) => {
+  var sortCondition = {};
+  if (req.query.search) {
+    const regex = new RegExp(searchRegularExpression(req.query.search), "gi");
+    Organizations.find({ $or: [{ name: regex }, { description: regex }] })
+      .sort(sortCondition)
+      .exec(function (err, orgs) {
+        if (err) throw err;
+        else {
+          res.render("explore", { user: req.user, orgs: orgs });
         }
-    })
-})
+      });
+  } else {
+    Organizations.find().exec(function (err, orgs) {
+      if (err) throw err;
+      else {
+        res.render("explore", { orgs });
+      }
+    });
+  }
+});
 
-router.get('/additem',(req,res)=>{
-    name='Dwarka Palace'
-field='hotel'
-city='Delhi'
-address=`C991, Behind Maxfort School, Sector 7, Dwarka
+//add items
+
+router.get("/additem", (req, res) => {
+  name = "Dwarka Palace";
+  field = "hotel";
+  city = "Delhi";
+  address = `C991, Behind Maxfort School, Sector 7, Dwarka
 Phone:099710 54499
 
-`
-description=`luxurious rooms with a great spread of breakfast`
+`;
+  description = `luxurious rooms with a great spread of breakfast`;
 
-const newOrg=new Organizations({name,description,field,city,address});
-newOrg.save();
-res.send(name+' successfully added');
-})
+  const newOrg = new Organizations({ name, description, field, city, address });
+  newOrg.save();
+  res.send(name + " successfully added");
+});
 
-//search post request
-
-// router.post('/search',ensureAuthenticated,(req,res=>{
-
-// }))
+function searchRegularExpression(searchQuery) {
+  return searchQuery.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
 
 
-
-
-//giving ratings
-
-router.post('/:id',ensureAuthenticated,(req,res)=>{
-    const user=req.user;
-    const organization=mongoose.Types.ObjectId(req.params.id);
-    const value_for_money=req.body.value_for_money;
-    const staff_service=req.body.staff_service;
-    const review=req.body.review;
-    const newRating=new Rating({user,organization,value_for_money,staff_service,review})
-    newRating.save().then((rating)=>{
-        Organizations.findById(organization,function(err,org){
-                    if(err){
-                        throw err;
-                    }else{
-                        org.ratings.push(rating);
-                        org.save();
-                        req.flash('success_message','Rated successfully');
-                        res.redirect('/organizations/'+req.params.id);
-                    }
-                })
-        
-    }).catch((err)=>console.log(err));
-})
-
+router.post("/info/:id", ensureAuthenticated, (req, res) => {
+  const user = req.user;
+  const organization = mongoose.Types.ObjectId(req.params.id);
+  const value_for_money = req.body.value_for_money;
+  const staff_service = req.body.staff_service;
+  const review = req.body.review;
+  const newRating = new Ratings({
+    user,
+    organization,
+    value_for_money,
+    staff_service,
+    review,
+  });
+  newRating
+    .save()
+    .then((rating) => {
+      Organizations.findById(organization, function (err, org) {
+        if (err) {
+          throw err;
+        } else {
+          org.ratings.push(rating);
+          org.save();
+          req.flash("success_message", "Rated successfully");
+          res.redirect("/organizations/" + req.params.id);
+        }
+      });
+    })
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
